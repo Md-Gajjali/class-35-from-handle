6f6ad1638313b08fe497624a2a4307077080f522
Md-Gajjali <hellogajjali@gmail.com>
2025-11-12 13:21:05 +0600

    add input error

--- src/App.jsx
+++ src/App.jsx
@@ -23,10 +23,15 @@ function App() {
 
   const[email,setEmail] = useState("")
   const [password,setPassword] = useState("")
+  const [error,setError] = useState("")
 
   const handleClick = (e) => {
     e.preventDefault()
-    console.log(email,password);
+    if (email == "") {
+      setError("boka coda input ta full fill kr");
+    } else {
+      setError("sabbas beda ");
+    }
   }
 
 
@@ -40,6 +45,7 @@ function App() {
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
             <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" onChange={(e)=> setEmail(e.target.value)}  />
           </div>
+          <p className='text-red-400 text-4xl '>{error}</p>
           <div>
             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
             <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  />
@@ -59,7 +65,6 @@ function App() {
           </div>
         </form>
       </div>
-
     </>
   )
 }
